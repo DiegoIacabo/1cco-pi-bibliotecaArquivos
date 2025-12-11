d3f3d3046079c1f7f6ff299f7fd565c91038998a
VinnyMendess <vinicius.msilva@sptech.school>
2025-12-11 10:36:01 -0300

    Fetch Estante

--- src/routes/estante.js
+++ src/routes/estante.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var estanteController = require("../controllers/estanteController");
 
-router.get("/:empresaId", function (req, res) {
+router.get("/:fkEmpresa", function (req, res) {
   estanteController.buscarestantePorEmpresa(req, res);
 });
 
-router.post("/cadastrar", function (req, res) {
-  estanteController.cadastrar(req, res);
+router.post("/estante", function (req, res) {
+  estanteController.cadastrarEstante(req, res);
 })
 
 module.exports = router;
